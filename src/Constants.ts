58bcf07bec232cd8090621e53dcb78f2a6924294
amarnath666 <amarnathdhumal2001@gmail.com>
2025-02-05 16:35:02 +0530

    added https

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,3 +1,3 @@
 export const token = localStorage.getItem("authToken")
 
-export const BASE_URL = "https://app.jauharijeweller.com/api"
+export const BASE_URL = "https://app.jauharijeweller.com/api"  
